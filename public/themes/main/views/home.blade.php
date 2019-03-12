bd0b162c1c1d2d780d840a4959b28a9a320533f6
Nguyen Thanh <nguyen.thanh@alliedtechcamp.com>
2019-03-12 16:50:14 +0700

    update source

--- public/themes/main/views/home.blade.php
+++ public/themes/main/views/home.blade.php
@@ -15,13 +15,6 @@
 {!!Theme::asset()->container('footer')->usePath()->add('jquery.validate.min', 'js/jquery.validate.min.js', array('core-script'))!!}
 {!!Theme::asset()->container('footer')->usePath()->add('jquery.gritter.min', 'js/jquery.gritter.min.js', array('core-script'))!!}
 <section>
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-	(adsbygoogle = window.adsbygoogle || []).push({
-		google_ad_client: "ca-pub-6739685874678212",
-		enable_page_level_ads: true
-	});
-</script>
 <div class="mainpanel">
 {!!Theme::partial('headerbar', array('title' => 'Header'))!!}
 	<div class="container panelRegisterSite">
@@ -199,16 +192,6 @@
 					<li class="list-group-item"><i class="glyphicon glyphicon-check text-success"></i> Và còn nữa...</li>
 				</ul>
 			</div>
-		</div>
-		<div class="form-group mt-2">
-			<ins class="adsbygoogle"
-				 style="display:block"
-				 data-ad-client="ca-pub-6739685874678212"
-				 data-ad-slot="7536384219"
-				 data-ad-format="auto"></ins>
-			<script>
-				(adsbygoogle = window.adsbygoogle || []).push({});
-			</script>
 		</div>
 	</div>
 
