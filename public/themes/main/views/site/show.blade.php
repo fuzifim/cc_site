0c2dbc610644b58bb3ad655a362127af524d4bf9
MacOS13 <macos13@MacBook-Pro-cua-MacOS13.local>
2019-04-13 17:11:25 +0700

    update source

--- public/themes/main/views/site/show.blade.php
+++ public/themes/main/views/site/show.blade.php
@@ -2,21 +2,7 @@
 Theme::setTitle($site['title']);
 Theme::setDescription($site['description']);
 Theme::setCanonical(route('site.show.id',array($channel['domainPrimary'],$site['_id'],str_slug(mb_substr($site['title'], 0, \App\Model\Mongo_site::MAX_LENGTH_SLUG),'-'))));
-$ads='true';
-if(!empty($domain['attribute']['ads']) && $domain['attribute']['ads']=='disable'){
-    $ads='false';
-}else if($domain['status']=='blacklist' && $domain['status']=='disable' && $domain['status']=='delete'){
-    $ads='false';
-}
-@if($ads=='true' && config('app.env')!='local')
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "ca-pub-6739685874678212",
-            enable_page_level_ads: true
-        });
-    </script>
-@endif
+
 ?>
 <section>
     <div class="mainpanel">
@@ -44,18 +30,6 @@ if(!empty($domain['attribute']['ads']) && $domain['attribute']['ads']=='disable'
                 <div class="panel-body">
                     <span>{!! $site['description'] !!}</span><br>
                     <span>{!! $site['link'] !!}</span><br>
-                    @if($ads=='true' && config('app.env')!='local')
-                        <div class="form-group">
-                            <ins class="adsbygoogle"
-                                 style="display:block"
-                                 data-ad-client="ca-pub-6739685874678212"
-                                 data-ad-slot="7536384219"
-                                 data-ad-format="auto"></ins>
-                            <script>
-                                (adsbygoogle = window.adsbygoogle || []).push({});
-                            </script>
-                        </div>
-                    @endif
                     <a class="btn btn-primary btn-block" id="" href="{!! route('go.to.url',array(config('app.url'),urlencode($site['link']))) !!}" rel="nofollow" target="_blank">Visit to {!! $site['title'] !!}
                         <p><strong>Click here</strong></p>
                     </a>
