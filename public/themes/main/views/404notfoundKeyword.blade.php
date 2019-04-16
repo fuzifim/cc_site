01e5521e71fbebe91f38595ea930d630e503db97
Nguyen Thanh <nguyen.thanh@alliedtechcamp.com>
2019-04-16 10:58:46 +0700

    update source

--- public/themes/main/views/404notfoundKeyword.blade.php
+++ public/themes/main/views/404notfoundKeyword.blade.php
@@ -1,4 +1,5 @@
 <?
+	Theme::setNoindex('noindex');
 	Theme::setTitle($keyword); 
 	Theme::setSearch($keyword); 
 	Theme::setCanonical(str_replace(' ', '+', urldecode(route('keyword.show',array(config('app.url'),preg_replace('/([+])\\1+/', '$1',rtrim(str_replace(' ', '+', preg_replace('/[^\w\s]+/u',' ' ,$keyword)), '+'))))))); 
@@ -9,7 +10,6 @@
 	Theme::asset()->container('footer')->usePath()->add('bootstrap', 'js/bootstrap.min.js', array('core-script'));
 	Theme::asset()->container('footer')->usePath()->add('modernizr', 'js/modernizr.min.js', array('core-script'));
 ?>
-<meta name="revisit-after" content="1 days">
 <section>
 <div class="mainpanel">
 {!!Theme::partial('headerbar', array('title' => 'Header'))!!}
