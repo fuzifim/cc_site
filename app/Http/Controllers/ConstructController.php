ae3a707e4cfec00526109359341916a18ddb42d9
MacOS13 <macos13@MacBook-Pro.local>
2019-04-27 11:09:30 +0700

    update source

--- app/Http/Controllers/ConstructController.php
+++ app/Http/Controllers/ConstructController.php
@@ -146,7 +146,7 @@ class ConstructController extends Controller
         }
         $this->_domainParentPrimary = config('app.url');
 		if($this->_channel->channel_parent_id==0){
-		    if($this->_domainPrimary!=config('app.url') && empty($this->_domain->domain)){
+		    if($this->_domainPrimary!=config('app.url')){
                 return redirect()->to('https://'.config('app.url').Request::getRequestUri())->send();
             }
             $this->_newKeyword=Cache::store('memcached')->remember('newKeyword', 1, function()
