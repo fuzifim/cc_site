25de1f39a3df928f40ba30c760a83bb0bde31451
Nguyen Thanh <nguyen.thanh@alliedtechcamp.com>
2019-07-19 10:34:50 +0700

    fix code

--- public/themes/main/partials/header.blade.php
+++ public/themes/main/partials/header.blade.php
@@ -40,5 +40,13 @@
 			</script>
 		@endif
     </head>
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.11&appId=1506437109671064&autoLogAppEvents=1';
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
     <body class="">
 	<!-- Preloader -->
