cdf7bce4819bae716894e44395b7ecb15622e2ce
Nguyen Thanh <nguyen.thanh@alliedtechcamp.com>
2019-07-22 11:46:17 +0700

    fix forgot password

--- public/themes/control/views/admin/user/forgotPasswordReset.blade.php
+++ public/themes/control/views/admin/user/forgotPasswordReset.blade.php
@@ -15,44 +15,53 @@
 <div class="mainpanel">
 {!!Theme::partial('headerbar', array('title' => 'Header'))!!}
 	<div class="pageheader">
-		<h1>Xác nhận quên mật khẩu</h1>
+		<h1>{{ __('base.password_reset') }}</h1>
 		<span><small>{!! Theme::get('description') !!}</small></span>
 	</div>
 	<div class="contentpanel">
-		<form class="formForgot" method="post" action="{{route("forgot.password.reset.request",$channel["domainPrimary"])}}">
+		<form class="formForgot" method="post" action="{{ route('password.update',config('app.url')) }}">
+			@csrf
+			<input type="hidden" name="token" value="{{ $token }}">
 			<div class="form-group">
 				<a href="{{route('channel.login',$channel['domainPrimary'])}}"><i class="glyphicon glyphicon-chevron-left"></i> Trở về trang đăng nhập</a>
 			</div>
-			<div class="panel panel-default mainbox"> 
-				<div id="preloaderInBox">
-					<div id="status"><i class="fa fa-spinner fa-spin"></i></div>
-				</div>
+			<div class="panel panel-default mainbox">
 				<div class="panel-body">
 					<div class="form-group">
 						<label class="control-label" for="email">Địa chỉ email</label>
 						<div class="input-group">
 							<span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-							<input id="email" name="email" value="{{$infoToken->email}}" type="email" class="form-control" placeholder="" disabled>
+							<input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" placeholder="Nhập địa chỉ email" required autofocus>
+							@if ($errors->has('email'))
+								<span class="invalid-feedback" role="alert">
+									<strong>{{ $errors->first('email') }}</strong>
+								</span>
+							@endif
 						</div>
 					</div>
 					<div class="form-group">
 						<label class="control-label" for="passwordNew">Mật khẩu mới</label>
 						<div class="input-group">
 							<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-							<input id="passwordNew" name="passwordNew" value="" type="password" class="form-control" placeholder="Nhập mật khẩu mới">
+							<input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Nhập mật khẩu mới" required>
+							@if ($errors->has('password'))
+								<span class="invalid-feedback" role="alert">
+									<strong>{{ $errors->first('password') }}</strong>
+								</span>
+							@endif
 						</div>
 					</div>
 					<div class="form-group">
 						<label class="control-label" for="rePasswordNew">Nhập lại mật khẩu</label>
 						<div class="input-group">
 							<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-							<input id="rePasswordNew" name="rePasswordNew" value="" type="password" class="form-control" placeholder="Nhập lại mật khẩu mới">
+							<input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Nhập lại mật khẩu mới" required>
 						</div>
 					</div>
 				</div>
 				<div class="panel-footer text-right">
 					<button id="changePasswordNew" class="btn btn-primary" type="submit"><i class="glyphicon glyphicon-ok"></i> Lưu </button>
-				</div>					
+				</div>
 			</div>
 		</form>
 	</div>
