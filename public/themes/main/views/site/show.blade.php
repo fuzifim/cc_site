6001b35d20ce19aa162349b2324f269f5afc415c
Nguyen Thanh <nguyen.thanh@alliedtechcamp.com>
2019-09-11 13:24:53 +0700

    fix btn goto youtube

--- public/themes/main/views/site/show.blade.php
+++ public/themes/main/views/site/show.blade.php
@@ -101,7 +101,7 @@ if($ads=='true' && config('app.env')!='local'){
             <div class="modal-body text-center">
                 <p>Nhấn vào nút <strong>thích trang</strong> để thấy nội dung <strong>{!! $site['title_full'] !!}</strong> </p>
                 <div class="fb-page" data-href="https://www.facebook.com/cungcap.net/" data-tabs="" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/cungcap.net/" class="fb-xfbml-parse-ignore"></blockquote></div>
-                <p>Hoặc nhấn <a href="https://www.youtube.com/channel/UCTR65Hn65TWPupGBWUMkzuA?sub_confirmation=1" target="_blank" rel="nofollow" class="btn btn-sm btn-success"><i class="glyphicon glyphicon-hand-right"></i> vào đây</a> và sau đó xác nhận đăng ký kênh bấm vào <strong>Đăng ký</strong> để xem nội dung <strong>{!! $site['title_full'] !!}</strong></p>
+                <p>Hoặc nhấn <a href="https://www.youtube.com/channel/UCTR65Hn65TWPupGBWUMkzuA?sub_confirmation=1" target="_blank" rel="nofollow" class="label label-success"><i class="glyphicon glyphicon-hand-right"></i> vào đây</a> và sau đó xác nhận đăng ký kênh bấm vào <strong>Đăng ký</strong> để xem nội dung <strong>{!! $site['title_full'] !!}</strong></p>
             </div>
         </div>
     </div>
